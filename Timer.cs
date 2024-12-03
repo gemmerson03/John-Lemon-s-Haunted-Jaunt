4fff94ae164aae943b92b04e4bc5d4d4847f062a
Grace Emmerson <gemmerson03@gmail.com>
2024-12-02 20:29:54 -0500

    first commit - committing all files to the repository

---
+++ Timer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float timeRemaining;
+    private bool timerIsRunning = false;
+    public GameEnding gameEnding;
+
+    private void Start()
+    {
+        timerIsRunning = true;
+    }
+  void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                gameEnding.CaughtPlayer();
+            }
+        }
+    }
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
